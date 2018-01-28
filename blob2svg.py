72947b536832b26f7e63a4c5921d27be93854d79
eyaler <eyalgruss@gmail.com>
2018-01-28 13:57:27 +0200

    added polybreak, code cleanup

--- blob2svg.py
+++ blob2svg.py
@@ -3,30 +3,67 @@ import cv2
 from itertools import groupby
 from time import time
 
+
 def get_area(contour, i):
-    p1 = contour[i-1][0]
-    p2 = contour[i][0]
-    p3 = contour[i+1 if i+1<len(contour) else 0][0]
-    return abs((p2[0]-p1[0])*(p3[1]-p1[1])-(p3[0]-p1[0])*(p2[1]-p1[1]))/2
+    p1 = contour[i - 1, 0]
+    p2 = contour[i, 0]
+    p3 = contour[i + 1 if i + 1 < len(contour) else 0, 0]
+    return abs((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])) / 2
+
 
 def vw_closed(contour, area):
-    contour = np.asarray([x[0] for x in groupby(contour.tolist())]) # remove consecutive duplicates - for performance
     areas = [get_area(contour, i) for i in range(len(contour))]
-    while len(contour)>2:
+    while len(contour) > 2:
         min_area = min(areas)
         if min_area > area:
             break
         i = np.argmin(areas)
         contour = np.delete(contour, i, axis=0)
         del areas[i]
-        if len(areas)>0:
-            areas[i-1] = get_area(contour, i-1)
-            j = i if i<len(areas) else 0
+        if len(areas):
+            areas[i - 1] = get_area(contour, i - 1)
+            j = i if i < len(areas) else 0
             areas[j] = get_area(contour, j)
     return contour
 
-def blob2svg(image, blob_levels=(1, 255), approx_method=None, simp_method='VW', abs_eps=0, rel_eps=0, min_area=0, box=False, erode_dilate_iters=0, erode_dilate_kernel=np.ones((3,3)), erode_dilate_connectivity=8, label=None, color=None,
-             save_to=None, save_png=False, png_bg_color=None, show=False, verbose=True):
+
+def polybreak(contour):
+    hull = cv2.convexHull(contour, returnPoints=False)
+    defects = cv2.convexityDefects(contour, hull)
+    if defects is None:
+        return [], []
+
+    internal_dist = 0
+    for i in range(len(defects)):
+        s, e, f, d = defects[i, 0]
+        if d >= internal_dist:
+            internal_dist = d
+            start_ind = s
+            end_ind = e
+            internal_ind = f
+            internal_point = contour[f, 0]
+
+    cut_dist = float('inf')
+    i = (end_ind + 1) % len(contour)
+    while i != start_ind:
+        d = np.linalg.norm(internal_point - contour[i, 0])
+        if d < cut_dist:
+            cut_dist = d
+            external_ind = i
+        i = (i + 1) % len(contour)
+    if cut_dist == float('inf'):
+        return [], []
+
+    cyclic = np.concatenate([contour] * 2)
+    contour1 = cyclic[external_ind:internal_ind + 1 + len(contour) * (internal_ind < external_ind)]
+    contour2 = cyclic[internal_ind:external_ind + 1 + len(contour) * (external_ind < internal_ind)]
+    return [contour1, contour2], [tuple(internal_point), tuple(contour[external_ind, 0])]
+
+
+def blob2svg(image, blob_levels=(1, 255), approx_method=None, simp_method='VW', abs_eps=0, rel_eps=0, min_area=0,
+             box=False, box_min_frac=0, erode_dilate_iters=0, erode_dilate_kernel=np.ones((3, 3)),
+             erode_dilate_connectivity=8, label=None, color=None,
+             save_to=None, save_png=False, png_bg_color=None, show=0, verbose=True):
     # approx_method can be one of: cv2.CHAIN_APPROX_NONE, cv2.CHAIN_APPROX_SIMPLE (or None, default), cv2.CHAIN_APPROX_TC89_L1, cv2.CHAIN_APPROX_TC89_KCOS
     # simp_method can be one of: 'RDP', 'VW'
     # connectivity can be one of: 0, 4, 8
@@ -49,17 +86,18 @@ def blob2svg(image, blob_levels=(1, 255), approx_method=None, simp_method='VW', 
     assert len(blob_levels) == 2
     image = np.uint8((image >= min(blob_levels)) * (image <= max(blob_levels)) * 255)
 
-    if erode_dilate_iters>0:
+    if erode_dilate_iters:
         eroded = cv2.erode(image, erode_dilate_kernel, iterations=erode_dilate_iters)
-        if erode_dilate_connectivity in (4,8):
+        if erode_dilate_connectivity in (4, 8):
             num, labels = cv2.connectedComponents(eroded, connectivity=erode_dilate_connectivity)
-            one_hot = (np.arange(1,num) == labels[..., np.newaxis]).astype(np.uint8)*255
-        elif erode_dilate_connectivity==0:
+            one_hot = (np.arange(1, num) == labels[..., np.newaxis]).astype(np.uint8) * 255
+        elif erode_dilate_connectivity == 0:
             num = 2
             one_hot = eroded[..., np.newaxis]
         else:
             raise ValueError
-        comps = [cv2.dilate(one_hot[..., i], erode_dilate_kernel, iterations=erode_dilate_iters) for i in range(num-1)]
+        comps = [cv2.dilate(one_hot[..., i], erode_dilate_kernel, iterations=erode_dilate_iters) for i in
+                 range(num - 1)]
     else:
         comps = [image]
 
@@ -70,7 +108,8 @@ def blob2svg(image, blob_levels=(1, 255), approx_method=None, simp_method='VW', 
     contours = []
 
     for comp in comps:
-        zoom = cv2.resize(comp, dsize=None, fx=2, fy=2, interpolation=cv2.INTER_NEAREST)   # this is in order to get a scalable vertex-following contour instead of a pixel-following contour
+        zoom = cv2.resize(comp, dsize=None, fx=2, fy=2,
+                          interpolation=cv2.INTER_NEAREST)  # this is in order to get a scalable vertex-following contour instead of a pixel-following contour
 
         _, comp_all_contours, _ = cv2.findContours(zoom, mode=cv2.RETR_LIST, method=approx_method)
         _, comp_contours, _ = cv2.findContours(zoom, mode=cv2.RETR_EXTERNAL, method=approx_method)
@@ -89,41 +128,65 @@ def blob2svg(image, blob_levels=(1, 255), approx_method=None, simp_method='VW', 
         label = color
 
     contours.sort(key=lambda x: str(x))
+    break_points = []
     skipped_small = 0
-    for i in range(len(contours)):
-        contours[i] = (contours[i] + 1) // 2
+    broke_boxes = 0
+    i = -1
+    original_contours = len(contours)
+    while i < len(contours) - 1:
+        i += 1
+        if i < original_contours:
+            contours[i] = (contours[i] + 1) // 2
+
+        # remove consecutive duplicates
+        contours[i] = np.asarray([x[0] for x in groupby(contours[i].tolist())])
+        if len(contours[i]) > 1 and np.array_equal(contours[i][-1], contours[i][0]):
+            contours[i] = contours[i][:-1]
 
         if abs_eps or rel_eps:
             if simp_method == 'RDP':
-                contours[i] = cv2.approxPolyDP(contours[i], max(abs_eps, rel_eps * cv2.arcLength(contours[i], closed=True)), closed=True)
+                contours[i] = cv2.approxPolyDP(contours[i],
+                                               max(abs_eps, rel_eps * cv2.arcLength(contours[i], closed=True)),
+                                               closed=True)
             elif simp_method == 'VW':
                 contours[i] = vw_closed(contours[i], max(abs_eps, rel_eps * cv2.contourArea(contours[i])))
             else:
                 raise ValueError
 
-        if cv2.contourArea(contours[i]) < min_area:
+        area = cv2.contourArea(contours[i])
+
+        if area < min_area:
             skipped_small += 1
             contours[i] = None
             continue
 
         if box:
-            contours[i] = cv2.boxPoints(cv2.minAreaRect(contours[i]))[:, None, :]
+            box_contour = cv2.boxPoints(cv2.minAreaRect(contours[i]))[:, None, :]
+            if area < box_min_frac * cv2.contourArea(box_contour):
+                sub_contours, bps = polybreak(contours[i])
+                if len(sub_contours):
+                    broke_boxes += 1
+                    contours += sub_contours
+                    break_points.append(bps)
+                    contours[i] = None
+                    continue
+            contours[i] = box_contour
 
-        # remove consecutive duplicates
-        contour = [x[0][0] for x in groupby(contours[i].tolist())]
-        if len(contour) > 1 and contour[-1] == contour[0]:
-            del contour[-1]
-
-        if len(contour)<3:
+        if len(contours[i]) < 3:
             skipped_small += 1
             contours[i] = None
             continue
 
-        points = ' '.join(str(float(p[0])).rstrip('0').rstrip('.')+','+str(float(p[1])).rstrip('0').rstrip('.') for p in contour)
+        points = ' '.join(
+            str(float(p[0, 0])).rstrip('0').rstrip('.') + ',' + str(float(p[0, 1])).rstrip('0').rstrip('.') for p in
+            contours[i])
         svg.append('<polygon class="%s" fill="%s" id="%d" points="%s"/>' % (label, color, i, points))
 
-    if skipped_small and verbose:
-        print('Skipped %d small contours'%(skipped_small))
+    if verbose:
+        if skipped_small:
+            print('Skipped %d small contours' % (skipped_small))
+        if broke_boxes:
+            print('Broke %d boxes' % (broke_boxes))
 
     if save_to is not None:
         save_svg(save_to, svg, resolution=image.shape[::-1])
@@ -134,7 +197,18 @@ def blob2svg(image, blob_levels=(1, 255), approx_method=None, simp_method='VW', 
         print('%.1f sec' % (time() - start_time))
         cimage = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
         cimage = cv2.copyMakeBorder(cimage, 0, 1, 0, 1, borderType=cv2.BORDER_CONSTANT)
-        cv2.drawContours(cimage, [np.int0(c) for c in contours if c is not None], -1, color=(0, 0, 255))
+        if show<=2:
+            cv2.drawContours(cimage, [np.int0(c) for c in contours if c is not None], -1, (0, 0, 255))
+            if show == 2:
+                for bps in break_points:
+                    cv2.circle(cimage, bps[0], 4, (0, 255, 0), thickness=2)
+                    cv2.circle(cimage, bps[1], 4, (255, 0, 0), thickness=2)
+        elif show==3:
+            for c in contours:
+                if c is not None:
+                    cv2.drawContours(cimage, [np.int0(c)], -1,
+                                     (np.random.randint(64, 256), np.random.randint(64, 256), np.random.randint(64, 256)),
+                                     thickness=-1)
         cv2.imshow('Found Contours', cimage)
         cv2.waitKey()
 
@@ -160,5 +234,6 @@ def svg2png(filename, bg_color=None):
             image.alpha_channel = 'remove'
         image.save(filename=filename + '.png')
 
+
 if __name__ == '__main__':
-    svg = blob2svg(image='test.png', save_to='test.svg', show=True)
+    svg = blob2svg(image='test.png', save_to='test.svg', show=2)
